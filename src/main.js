50be2423d857a98d225a9cf6fd48be4dc362c33f
wdNaimur <naimur.wd@gmail.com>
2025-08-16 17:00:30 +0600

    feat: initial commit

---
+++ src/main.js
@@ -0,0 +1,9 @@
+import './style.css'
+
+document.querySelector('#app').innerHTML = `
+  <div>
+   <h1>Welcome to Innovatech Solutions!</h1>
+  </div>
+`
+
+setupCounter(document.querySelector('#counter'))
