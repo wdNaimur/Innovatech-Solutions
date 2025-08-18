af18bc0d0fabb1dffe41cab1676ce112f7a5374e
wdNaimur <naimur.wd@gmail.com>
2025-08-18 10:55:02 +0600

    feat(navbar): implement responsive navbar with hamburger toggle functionality

--- src/main.js
+++ src/main.js
@@ -13,7 +13,31 @@ async function loadSection(targetId) {
   link.rel = "stylesheet";
   link.href = `/src/sections/${targetId}.css`;
   document.head.appendChild(link);
+
+  // toggle navbar 
+  if (targetId === "navbar") {
+    initNavbarToggle();
+  }
 }
+
+function initNavbarToggle() {
+  const hamburger = document.querySelector(".hamburger-icon");
+  const mobileNav = document.querySelector(".nav-links-mobile");
+
+  if (hamburger && mobileNav) {
+    hamburger.addEventListener("click", () => {
+      mobileNav.classList.toggle("active");
+
+
+      if (mobileNav.classList.contains("active")) {
+        hamburger.src = "/close.png";
+      } else {
+        hamburger.src = "/hamburger-icon.svg";
+      }
+    });
+  }
+}
+
 
 // ✅ Manually load each section
 loadSection("navbar");
